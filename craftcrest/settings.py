171e74c90f428368d54d21004a2e7eb295535686
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-09-26 11:41:05 +0300

    media root

--- craftcrest/settings.py
+++ craftcrest/settings.py
@@ -50,8 +50,8 @@ load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
